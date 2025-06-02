d5a27a67fa6e39ec6b7278c6fc7e53003ac80820
flora <balogh.flora@yahoo.com>
2025-06-02 15:12:32 +0300

    team members fixed

--- src/app/feature/profile/profile/profile.component.ts
+++ src/app/feature/profile/profile/profile.component.ts
@@ -5,11 +5,12 @@ import { AuthService } from '../../../core/services/auth/auth.service';
 import { UserService } from '../../../core/services/user/user.service';
 import { UserResponse } from '../models/user-response.model';
 import { Role } from '../models/user-role.enum';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, RouterLink], // Add RouterLink here
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
@@ -28,11 +29,24 @@ export class ProfileComponent implements OnInit, OnDestroy {
   ) { }
 
   ngOnInit(): void {
+    console.log('=== PROFILE COMPONENT INIT ===');
+
+    // First load from auth service
     this.userSubscription = this.authService.user$
       .pipe(filter(response => !!response))
       .subscribe(response => {
+        console.log('User from auth service:', response);
         this.user = response;
+
+        // If team info is missing, refresh from server
+        if (response && (!response.teamId || !response.teamName)) {
+          console.log('Team info missing, refreshing from server...');
+          this.refreshProfile();
+        }
       });
+
+    // Also refresh profile on init to ensure we have latest data
+    this.refreshProfile();
   }
 
   ngOnDestroy(): void {
@@ -53,10 +67,14 @@ export class ProfileComponent implements OnInit, OnDestroy {
   }
 
   refreshProfile(): void {
+    console.log('Refreshing profile from server...');
     this.loading = true;
+
     this.userService.getUserInfo().subscribe({
       next: (user) => {
-        this.authService.setUser(user);
+        console.log('Fresh user data from server:', user);
+        this.user = user;
+        this.authService.setUser(user); // Update the auth service with fresh data
         this.loading = false;
       },
       error: (error) => {
@@ -64,5 +82,14 @@ export class ProfileComponent implements OnInit, OnDestroy {
         this.loading = false;
       }
     });
+  }
+
+  // Debug method to check user data
+  debugUserData(): void {
+    console.log('=== USER DATA DEBUG ===');
+    console.log('Current user:', this.user);
+    console.log('Team ID:', this.user?.teamId);
+    console.log('Team Name:', this.user?.teamName);
+    console.log('Has team?', !!(this.user?.teamId && this.user?.teamName));
   }
 }
